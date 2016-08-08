95fdd1368275b308d194efcf08c62fb1d6657a27
Danny Hadley <dadleyy@gmail.com>
2016-08-08 14:26:51 -0400

    adding clients table and initial application client

---
+++ migrations/20160808135550_create_client_table.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const crypto   = require("crypto");
+const bluebird = require("bluebird");
+
+function key(desired_character_length) {
+  var byte_count = desired_character_length * 0.5;
+  return new bluebird(function(resolve, reject) {
+    crypto.randomBytes(byte_count, function(err, result) {
+      resolve(result.toString("hex"));
+    });
+  });
+}
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("clients", function (table) {
+    table.increments()
+    table.string("name").unique();
+    table.string("client_id", 15).unique();
+    table.string("client_secret", 40).unique();
+    table.timestamps();
+    table.dateTime("deleted_at");
+  }).then(function() {
+    return bluebird.props({
+      client_id     : key(20),
+      client_secret : key(40)
+    }).then(function(results) {
+      return knex("clients").insert({
+        name          : "meritoss",
+        client_id     : results.client_id,
+        client_secret : results.client_secret,
+        created_at    : new Date()
+      });
+    });
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("clients");
+};
