9e57c8d5bdc58d6e81dccec988baeadd1a886735
Danny Hadley <dadleyy@gmail.com>
2016-08-08 18:50:19 -0400

    correcting key lengths in client and token table

--- migrations/20160808135550_create_client_table.js
+++ migrations/20160808135550_create_client_table.js
@@ -16,7 +16,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("clients", function (table) {
     table.increments()
     table.string("name").unique();
-    table.string("client_id", 15).unique();
+    table.string("client_id", 20).unique();
     table.string("client_secret", 40).unique();
     table.timestamps();
     table.dateTime("deleted_at");
--- migrations/20160808142734_create_client_token_table.js
+++ migrations/20160808142734_create_client_token_table.js
@@ -1,10 +1,12 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("client_tokens", function (table) {
-    table.string("token", 15).unique();
+    table.increments()
+    table.string("token", 20).unique();
     table.integer("client").unsigned().references("clients.id").notNullable();
     table.integer("user").unsigned().references("users.id").notNullable();
     table.dateTime("deleted_at");
+    table.timestamps()
   });
 };
 
