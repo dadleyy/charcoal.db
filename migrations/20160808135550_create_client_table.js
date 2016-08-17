4e54a67d9198d9104fc665e2bbe49ff6b06da581
Danny Hadley <dadleyy@gmail.com>
2016-08-16 21:00:03 -0400

    adding convenience log for initial seed of meritoss client

--- migrations/20160808135550_create_client_table.js
+++ migrations/20160808135550_create_client_table.js
@@ -25,6 +25,14 @@ exports.up = function(knex, Promise) {
       client_id     : key(20),
       client_secret : key(40)
     }).then(function(results) {
+      console.log(`
+      Note: creating meritoss client! This information is essential
+      for getting started with the api locally.
+
+        client id     : ${results.client_id}
+        client secret : ${results.client_secret}
+
+      `);
       return knex("clients").insert({
         name          : "meritoss",
         client_id     : results.client_id,
