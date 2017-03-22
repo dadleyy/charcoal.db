5ba1909a6bc0c45257b5ef1a3c595cc505309790
Danny Hadley <dadleyy@gmail.com>
2017-03-22 16:44:13 -0400

    [CHAPI-55] dropping entry_round_id on game membership table

---
+++ migrations/20170322164122_drop_entry_round_id_membership_table.js
@@ -0,0 +1,19 @@
+const COLUMN = "entry_round_id";
+const TABLE = "game_memberships";
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table(TABLE, function(table) {
+    table.dropForeign(COLUMN);
+  }).then(function() {
+    return knex.schema.table(TABLE, function(table) {
+      table.dropColumn(COLUMN);
+    });
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table(TABLE, function(table) {
+    table.integer(COLUMN).unsigned().references("game_rounds.id").nullable();
+  });
+
+};
