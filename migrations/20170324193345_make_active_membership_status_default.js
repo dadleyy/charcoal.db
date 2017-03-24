646b13edfb28da11e45b2c38e0bed62cd77b5591
Danny Hadley <dadleyy@gmail.com>
2017-03-24 19:37:14 -0400

    [CHAPI-62] making active status the default on mem table

---
+++ migrations/20170324193345_make_active_membership_status_default.js
@@ -0,0 +1,8 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.raw("ALTER TABLE game_memberships ALTER COLUMN status SET DEFAULT 'ACTIVE'");
+};
+
+exports.down = function(knex, Promise) {
+  
+};
